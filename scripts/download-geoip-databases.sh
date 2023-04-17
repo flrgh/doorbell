3f72fef7ef0d59ff2bdd48ffab3d13a2b18f0042
Michael Martin <3277009+flrgh@users.noreply.github.com>
2023-04-17 11:28:35 -0700

    fix(ci): do not update in unpack step

--- scripts/download-geoip-databases.sh
+++ scripts/download-geoip-databases.sh
@@ -8,15 +8,13 @@ readonly GEOIP_CITY=${DOWNLOAD}/GeoLite2-City.tar.gz
 readonly GEOIP_COUNTRY=${DOWNLOAD}/GeoLite2-Country.tar.gz
 readonly BASE_URL=https://download.maxmind.com/app/geoip_download
 
-readonly DOWNLOAD_ONLY=${DOWNLOAD_ONLY:-0}
-readonly UPDATE=${UPDATE:-1}
+readonly NO_DOWNLOAD=${NO_DOWNLOAD:-0}
+readonly NO_UNPACK=${NO_UNPACK:-0}
+
 mkdir -p "$DOWNLOAD"
 
-UPDATED=0
-
-if [[ $UPDATE == 1 || $DOWNLOAD_ONLY == 1 || ! -e $GEOIP_CITY ]]; then
+if (( NO_DOWNLOAD == 0 )); then
     echo "Downloading GeoIP City DB"
-
     curl \
         --fail \
         --silent \
@@ -25,12 +23,7 @@ if [[ $UPDATE == 1 || $DOWNLOAD_ONLY == 1 || ! -e $GEOIP_CITY ]]; then
         --output "$GEOIP_CITY" \
         --url "${BASE_URL}?edition_id=GeoLite2-City&license_key=${MAXMIND_LICENSE_KEY}&suffix=tar.gz"
 
-    UDPATED=1
-fi
-
-if [[ $UPDATE == 1 || $DOWNLOAD_ONLY == 1 || ! -e $GEOIP_COUNTRY ]]; then
     echo "Downloading GeoIP Country DB"
-
     curl \
         --fail \
         --silent \
@@ -38,11 +31,9 @@ if [[ $UPDATE == 1 || $DOWNLOAD_ONLY == 1 || ! -e $GEOIP_COUNTRY ]]; then
         --time-cond "$GEOIP_COUNTRY" \
         --output "$GEOIP_COUNTRY" \
         --url "${BASE_URL}?edition_id=GeoLite2-Country&license_key=${MAXMIND_LICENSE_KEY}&suffix=tar.gz"
-
-    UDPATED=1
 fi
 
-if (( UPDATED == 1 && DOWNLOAD_ONLY == 0 )); then
+if (( NO_UNPACK == 0 )); then
     echo "Unpacking GeoIP City DB"
     tar xf "$GEOIP_CITY" \
       -C "$GEOIP" \
