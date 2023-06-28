b82c2799dff3140d7607b09221dfb1ddd12fe17b
Michael Martin <flrgh@protonmail.com>
2023-06-28 12:23:23 -0700

    tests(unit): increase coverage of doorbell.util

--- spec/01-unit/04-util_spec.lua
+++ spec/01-unit/04-util_spec.lua
@@ -1,4 +1,6 @@
 local util = require "doorbell.util"
+local cjson = require "cjson"
+local lfs = require "lfs"
 
 describe("doorbell.util", function()
   describe("current_time()", function()
@@ -83,6 +85,121 @@ describe("doorbell.util", function()
     it("sorts its output", function()
       assert.same({ "a", "b" }, util.table_keys({ b = 1, a = 2 }))
     end)
+  end)
 
+  describe("sha256()", function()
+    it("returns a sh256 checksum in hexadecimal form", function()
+      assert.same("c0ddd62c7717180e7ffb8a15bb9674d3ec92592e0b7ac7d1d5289836b4553be2",
+                  util.sha256("hi!"))
+    end)
+  end)
+
+  describe("split_at_comma()", function()
+    it("splits a string into a table", function()
+      assert.same({"a", "b", "c"}, util.split_at_comma("a,b,c"))
+    end)
+
+    it("strips whitespace from each item", function()
+      assert.same({"a", "b", "c"}, util.split_at_comma(" a ,b,   c"))
+    end)
+
+    it("returns a table with a single item when no comma is found", function()
+      assert.same({"abc"}, util.split_at_comma("abc"))
+    end)
+
+    it("sets the cjson array metatable on the output", function()
+      assert.equals(cjson.array_mt, getmetatable(util.split_at_comma("a,b,c")))
+    end)
+  end)
+
+  describe("join()", function()
+    it("joins filesystem path elements", function()
+      assert.equals("a/b/c", util.join("a", "b", "c"))
+    end)
+
+    it("does not produce duplicate separators", function()
+      assert.equals("a/b/c", util.join("a/", "/b/", "c"))
+    end)
+
+    it("does not produce a trailing separator", function()
+      assert.equals("a/b/c", util.join("a/", "/b/", "c/"))
+      assert.equals("a/b/c", util.join("a/", "/b/", "c////"))
+
+      -- eh, not so sure about this one
+      assert.equals("a/b/c", util.join("a/", "/b/", "c", ""))
+    end)
+
+    it("does not mangle args with multiple path elements", function()
+      assert.equals("a/b/subdir/c", util.join("a", "b/subdir/", "c"))
+    end)
+
+    it("does not mangle relative paths", function()
+      assert.equals("./a/b/c", util.join("./a", "b/c"))
+      assert.equals("./a/b/c", util.join(".//a", "b/c"))
+      assert.equals("./a/b/../c", util.join(".//a", "b", "..", "c"))
+      assert.equals("./a/b/../c", util.join(".//a", "b", "../", "c"))
+      assert.equals("./a/b/../c", util.join(".//a", "b", "../", "/c/"))
+      assert.equals("./a/b/../c", util.join(".//a//", "b", "../c/"))
+    end)
+
+
+    it("handles absolute paths", function()
+      assert.equals("/a/b/c", util.join("/a", "b", "c"))
+      assert.equals("/a/b/c", util.join("/", "a", "b", "c"))
+    end)
+  end)
+
+  describe("update_json_file()", function()
+    it("writes json to a file", function()
+      local f = os.tmpname()
+      assert(util.update_json_file(f, { a = 1, b = 2 }))
+      assert.same({ a = 1, b = 2 }, util.read_json_file(f))
+    end)
+
+    it("only writes the file if changes are made", function()
+      local f = os.tmpname()
+      local j = { a = 1, b = 2 }
+      assert(util.update_json_file(f, j))
+      assert.same(j, util.read_json_file(f))
+
+      local init = lfs.attributes(f)
+
+      local ok, err, written = util.update_json_file(f, j)
+      assert.is_nil(err)
+      assert.truthy(ok)
+      assert.is_false(written)
+      assert.same(j, util.read_json_file(f))
+
+      local no_update = lfs.attributes(f)
+      assert.equals(init.ino, no_update.ino)
+      assert.equals(init.modification, no_update.modification)
+
+      j.c = 3
+      ok, err, written = util.update_json_file(f, j)
+      assert.is_nil(err)
+      assert.truthy(ok)
+      assert.is_true(written)
+      assert.same(j, util.read_json_file(f))
+
+      local update = lfs.attributes(f)
+      assert.not_equals(init.ino, update.ino)
+      -- not doing a mod time check here because I don't want to
+      -- add a sleep to this test suite
+      -- assert.not_equals(init.modification, update.modification)
+
+      ok, err, written = util.update_json_file(f, j)
+      assert.is_nil(err)
+      assert.truthy(ok)
+      assert.is_false(written)
+      assert.same(j, util.read_json_file(f))
+
+      assert(util.write_file(f, "I've changed!"))
+
+      ok, err, written = util.update_json_file(f, j)
+      assert.is_nil(err)
+      assert.truthy(ok)
+      assert.is_true(written)
+      assert.same(j, util.read_json_file(f))
+    end)
   end)
 end)
