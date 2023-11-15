d58e485a22174f292eac68b8cc7089b2f34df04d
Michael Martin <flrgh@protonmail.com>
2023-11-14 17:57:59 -0800

    wip

--- src/database.rs
+++ src/database.rs
@@ -1,6 +1,7 @@
 use actix_web::{error, web, Error};
 use anyhow::Result;
 
+use crate::rules::Rule;
 use r2d2_sqlite::rusqlite;
 use r2d2_sqlite::rusqlite::Statement;
 use serde::{Deserialize, Serialize};
@@ -30,9 +31,12 @@ fn set_meta<T>(conn: &rusqlite::Connection, key: &str, value: T)
 where
     T: std::string::ToString,
 {
-    let mut stmt = conn.prepare("INSERT INTO meta (key, value) VALUES (?1, ?2) ON CONFLICT(key) DO UPDATE SET value = excluded.value").unwrap();
-
-    stmt.execute([key, &value.to_string()]).unwrap();
+    conn.execute(
+        "INSERT INTO meta (key, value) VALUES (?1, ?2)
+        ON CONFLICT(key) DO UPDATE SET value = excluded.value",
+        [key, &value.to_string()],
+    )
+    .unwrap();
 }
 
 fn init(db: &std::path::PathBuf) {
@@ -44,7 +48,7 @@ fn init(db: &std::path::PathBuf) {
     assert!(version <= MIGRATIONS.len());
 
     for (i, m) in MIGRATIONS.iter().skip(version).enumerate() {
-        eprintln!("Running migration {}", i);
+        eprintln!("Running migration {i}");
         conn.execute_batch(m).unwrap();
         set_meta(&conn, "db_version", i + 1);
     }
@@ -55,5 +59,44 @@ pub(crate) fn connect(db: &std::path::PathBuf) {
 
     let manager = r2d2_sqlite::SqliteConnectionManager::file(db);
     let pool = Pool::new(manager).unwrap();
+    let conn = pool.get().unwrap();
     dbg!(pool);
+}
+
+pub(crate) fn list_rules(conn: &Connection) {
+    let mut s = conn
+        .prepare(
+            "SELECT
+                id,
+                hash,
+                action,
+                deny_action,
+                terminate,
+                expires,
+                source,
+                comment,
+                created_at,
+                updated_at,
+                conditions
+            FROM rules",
+        )
+        .unwrap();
+
+    s.query_map([], |row| {
+        dbg!(&row);
+        Ok(Rule {
+            id: row.get(0)?,
+            hash: row.get(1)?,
+            action: row.get(2)?,
+            deny_action: row.get(3)?,
+            terminate: row.get(4)?,
+            expires: row.get(5)?,
+            source: row.get(6)?,
+            comment: row.get(7)?,
+            created_at: row.get(8)?,
+            updated_at: row.get(9)?,
+            conditions: row.get(10)?,
+        })
+    })
+    .unwrap();
 }
