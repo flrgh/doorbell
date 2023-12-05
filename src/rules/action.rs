c4449e4459980084c18310091bb86728c9289155
Michael Martin <flrgh@protonmail.com>
2023-12-05 13:35:04 -0800

    refactor

--- src/rules/action.rs
+++ src/rules/action.rs
@@ -21,25 +21,3 @@ pub enum Action {
     Deny,
     Allow,
 }
-
-#[derive(
-    PartialEq,
-    Eq,
-    Clone,
-    Debug,
-    Default,
-    strum_macros::Display,
-    strum_macros::EnumString,
-    strum_macros::EnumIs,
-    sqlx::Type,
-    serde_derive::Deserialize,
-    serde_derive::Serialize,
-)]
-#[strum(serialize_all = "lowercase")]
-#[sqlx(rename_all = "lowercase")]
-#[serde(rename_all = "lowercase")]
-pub enum DenyAction {
-    #[default]
-    Exit,
-    Tarpit,
-}
